3b9838c3222311bbbfe65066a329bd582cfb0c46
Han shenquan <1173956412@qq.com>
2016-04-25 22:09:31 +0800

    用Inflate方式成功实现了ViewPager
    用Inflate方式成功实现了ViewPager，并且可以动态的修改所加载的内容，注意加载Layout中的具体控件的方法。例如：TextView
    textView = (TextView)layoutView.findViewById(R.id.tv);

--- app/src/main/java/com/hansq/viewpagerinflate/MainActivity.java
+++ app/src/main/java/com/hansq/viewpagerinflate/MainActivity.java
@@ -1,12 +1,16 @@
 package com.hansq.viewpagerinflate;
 
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,33 +25,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewGroup viewGroup = (ViewGroup)findViewById(R.id.viewGroup);
-        mViewPager = (ViewPager)findViewById(R.id.viewPager);
+        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.viewGroup);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
         LayoutInflater layoutInflater = LayoutInflater.from(this);
-        //mCount是订单数量，是传过来的数据。在此处我们令其为List的长度
-        mCount = mLayouts.size();
+        //mCount是订单数量，是从订单处得到的数据，我们默认设为3
 
-        for(int i = 0;i<mCount;i++){
-            mLayouts.add(layoutInflater.inflate(R.layout.one,null));
-            mDots.add(layoutInflater.inflate(R.layout.dot,null));
-            viewGroup.addView(mDots.get(i));
-            if(i==0){
-                mDots.get(i).setBackgroundResource(R.drawable.dot_select);
-            }else{
-                mDots.get(i).setBackgroundResource(R.drawable.dot_no_select);
+        mLayouts = new ArrayList<>();
+        mDots = new ArrayList<>();
+        mCount = 3;
+        for (int i = 0; i < mCount; i++) {
+            View layoutView = layoutInflater.inflate(R.layout.one, null);
+            View dotView = layoutInflater.inflate(R.layout.dot, null);
+            mLayouts.add(layoutView);
+            mDots.add(dotView);
+
+            TextView textView = (TextView)layoutView.findViewById(R.id.tv);
+            textView.setText("我是TextView"+(i+1));
+            Button button =(Button)layoutView.findViewById(R.id.btn);
+            button.setText("我是Button"+(i+1));
+
+            if (i == 0) {
+                dotView.setBackgroundResource(R.drawable.dot_select);
+            } else {
+                dotView.setBackgroundResource(R.drawable.dot_no_select);
             }
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+            layoutParams.setMargins(20, 0, 20, 0);
+            viewGroup.addView(dotView, layoutParams);
         }
 
         mViewPager.setAdapter(new MyViewPagerAdapter(mLayouts));
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                
+
             }
 
             @Override
             public void onPageSelected(int position) {
-
+                for (int i = 0; i < mCount; i++) {
+                    if (i == position) {
+                        mDots.get(i).setBackgroundResource(R.drawable.dot_select);
+                    } else {
+                        mDots.get(i).setBackgroundResource(R.drawable.dot_no_select);
+                    }
+                }
             }
 
             @Override
@@ -55,8 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
 
     }
 }
